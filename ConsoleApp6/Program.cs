fe8068858985f7e8ee6d7bad13e368d009a1986d
VanekAndreev2005 <anda18276@gmail.com>
2022-11-22 14:09:11 +0900

    pr22.11

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое целое число");
+            try
+            {
+                double i = int.Parse(Console.ReadLine());
+                int x = 5;
+                double y = x / i;
+                Console.WriteLine($"{x}/{i}={y}");
+            }
+            catch
+            {
+                Console.WriteLine("Неправильный ввод значения");
+            }
+            finally
+            {
+                Console.WriteLine("Выполнен блок finally");
+            }
+                Console.ReadLine();
+        }
+    }
+}
